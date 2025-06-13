5af9df79f4f5d42f091bd7b3545cccf08ee71b12
Robert Forehand <ra.forehand@gmail.com>
2025-06-12 19:16:55 -0700

    [MAIN] Adding Mob scene and executing the random spawn

---
+++ Main.cs
@@ -0,0 +1,32 @@
+using Godot;
+using System;
+
+public partial class Main : Node
+{
+	[Export]
+	private PackedScene _mobScene { get; set; }
+	
+	private PathFollow3D _mobSpawnLocation;
+	private Player _player;
+	
+	private void OnMobTimerTimeout()
+	{
+		Mob mob = _mobScene.Instantiate<Mob>();
+		_mobSpawnLocation.ProgressRatio = GD.Randf();
+		mob.Initialize(_mobSpawnLocation.Position, _player.Position);
+		
+		AddChild(mob);
+	}
+	
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		_player = GetNode<Player>("Player");
+		_mobSpawnLocation = GetNode<PathFollow3D>("SpawnPath/SpawnLocation");
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}
